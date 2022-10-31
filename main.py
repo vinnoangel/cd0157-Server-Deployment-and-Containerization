66679b57f69b5e802f3c44d6f8600abf1584041c
Vincent <vinnoangel@gmail.com>
2022-10-31 18:55:59 +0100

    added error handler file

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ import jwt
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
+from http_status_codes import *
+from error_handler import http_error_handler
+
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
@@ -72,10 +75,10 @@ def auth():
     password = request_data.get('password')
     if not email:
         LOG.error("No email provided")
-        return jsonify({"message": "Missing parameter: email"}, 400)
+        return http_error_handler(error=HTTP_400_BAD_REQUEST, message="Missing parameter: email")
     if not password:
         LOG.error("No password provided")
-        return jsonify({"message": "Missing parameter: password"}, 400)
+        return http_error_handler(error=HTTP_400_BAD_REQUEST, message="Missing parameter: password")
     body = {'email': email, 'password': password}
 
     user_data = body
@@ -89,13 +92,13 @@ def decode_jwt():
     Check user token and return non-secret data
     """
     if not 'Authorization' in request.headers:
-        abort(401)
+        return http_error_handler(error=HTTP_401_UNAUTHORIZED, message="Authorization header is expected.")
     data = request.headers['Authorization']
     token = str.replace(str(data), 'Bearer ', '')
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
     except: # pylint: disable=bare-except
-        abort(401)
+        return http_error_handler(error=HTTP_401_UNAUTHORIZED, message="Invalid Token: Unauthorized user")
 
 
     response = {'email': data['email'],
@@ -110,6 +113,48 @@ def _get_jwt(user_data):
                'nbf': datetime.datetime.utcnow(),
                'email': user_data['email']}
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+
+
+'''
+    error handler should conform to general task above
+'''
+
+@APP.errorhandler(HTTP_400_BAD_REQUEST)
+def bad_request(error):
+    return http_error_handler(HTTP_400_BAD_REQUEST, "bad request")
+
+@APP.errorhandler(HTTP_401_UNAUTHORIZED)
+def unauthorized(error):
+    return http_error_handler(HTTP_401_UNAUTHORIZED, "unauthorized")
+
+@APP.errorhandler(HTTP_403_FORBIDDEN)
+def forbidden(error):
+    return http_error_handler(HTTP_403_FORBIDDEN, "forbidden")
+
+@APP.errorhandler(HTTP_404_NOT_FOUND)
+def not_found(error):
+    return http_error_handler(HTTP_404_NOT_FOUND, "resource not found")
+    
+@APP.errorhandler(HTTP_405_METHOD_NOT_ALLOWED)
+def method_not_allowed(error):
+    return http_error_handler(HTTP_405_METHOD_NOT_ALLOWED, "method not allowed")
+    
+@APP.errorhandler(HTTP_408_REQUEST_TIMEOUT)
+def request_timeout(error):
+    return http_error_handler(HTTP_408_REQUEST_TIMEOUT, "request timeout")
+    
+@APP.errorhandler(HTTP_409_CONFLICT)
+def conflict(error):
+    return http_error_handler(HTTP_409_CONFLICT, "request conflicts")
+    
+@APP.errorhandler(HTTP_422_UNPROCESSABLE_ENTITY)
+def unprocessable(error):
+    return http_error_handler(HTTP_422_UNPROCESSABLE_ENTITY, "unprocessable")
+    
+@APP.errorhandler(HTTP_500_INTERNAL_SERVER_ERROR)
+def internal_server_error(error):
+    return http_error_handler(HTTP_500_INTERNAL_SERVER_ERROR, "internal server error")
+
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
